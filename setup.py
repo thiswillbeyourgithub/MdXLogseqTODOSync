001a38fbaa015670a401dd9c49cff5f543686e0a
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-11-29 17:20:06 +0100

    bump version 0.0.5 -> 0.0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="MdXLogseqTODOSync",
-    version="0.0.5",
+    version="0.0.6",
     description="Script to always sync a Logseq TODO list with a markdown file",
     long_description=long_description,
     long_description_content_type="text/markdown",
