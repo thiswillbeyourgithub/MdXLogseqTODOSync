45fe53cbc2c1ae6d05807c98a403a3f758ee2329
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-03-22 13:25:28 +0100

    minor
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- MdXLogseqTODOSync/__init__.py
+++ MdXLogseqTODOSync/__init__.py
@@ -11,7 +11,8 @@ __VERSION__ = MdXLogseqTODOSync.VERSION
 def cli_launcher() -> None:
     if sys.argv[-1] ==  "--version":
         return(f"MdXLogseqTODOSync version: {__VERSION__}")
-    fire.Fire(MdXLogseqTODOSync)
+    _ = fire.Fire(MdXLogseqTODOSync)
+    return None
 
 if __name__ == "__main__":
     cli_launcher()
